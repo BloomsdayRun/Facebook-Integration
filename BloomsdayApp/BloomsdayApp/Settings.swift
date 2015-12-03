2cbe9ce5283a36ff60f4e7e6cc7b95579d6c3c0a
Martin Johnsson <AlphaForceOne@gmail.com>
2015-12-03 09:46:59 -0800

    sdf

---
+++ BloomsdayApp/BloomsdayApp/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  BloomsdayApp
+//
+//  Created by Jason Schnagl on 12/1/15.
+//  Copyright © 2015 Senior Design. All rights reserved.
+//
+
+import Foundation
+
+class Settings: UIViewController {
+    
+}
+
